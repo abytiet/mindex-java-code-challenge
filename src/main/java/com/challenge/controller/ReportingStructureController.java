5eb1b5a25eb94f007dd58b38ae3814f26dbf5362
aby <abigailtiet@gmail.com>
2021-02-05 23:25:38 -0500

    basic objects created, reportingstructure service/serviceimpl/contrller base code

---
+++ src/main/java/com/challenge/controller/ReportingStructureController.java
@@ -0,0 +1,24 @@
+package com.mindex.challenge.controller;
+
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ReportingStructureReporter {
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureController.class);
+
+    @Autowired
+    private ReportingStructureService reportingStructureService;
+
+    @GetMapping("/reportingStructure/{id}")
+    public Employee read(@PathVariable String id) {
+        LOG.debug("Received reporting structure create request for id [{}]", id);
+
+        return reportingStructureService.read(id);
+    }
+
+}
